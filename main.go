75c4f75967b2d2ae41a9f287b5b48894bf230f39
AddisonRogers <87341723+AddisonRogers@users.noreply.github.com>
2022-08-21 08:29:38 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func init() {
 }
 
 func main() {
-	Token = ""
+	Token = "OTg4NTg3NzE1Mzc1MjcxOTY2.Gu1GuW.sDtRY6L_ewDUsiqKZnPRdvbCFdqtMh-RFVEHaU"
 
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
@@ -47,6 +47,12 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	fmt.Println(m.Content)
 	if m.Author.ID == s.State.User.ID {
 		return
+	}
+	if m.Content == "Fucking cunt" {
+		s.ChannelMessageSend(m.ChannelID, "im sad")
+	}
+	if m.Content == "Good bot" {
+		s.ChannelMessageSend(m.ChannelID, "im happy !")
 	}
 	if m.Content[0:1] == "." {
 		m.Content = m.Content[1:]
@@ -80,51 +86,56 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 //TODO add anime compatibility (GET BOWEN)
 
 func mainroute(s *discordgo.Session, m *discordgo.MessageCreate) {
-	channelID := m.ChannelID
-	content := m.Content
-	s.ChannelMessageSend(channelID, "Ok I will find this thing's magnet link")
+
+	s.ChannelMessageSend(m.ChannelID, "Ok I will find this thing's magnet link")
 	bow := surf.NewBrowser()
-	err := bow.Open(fmt.Sprintf("https://1337x.to/search/%s/1/", content))
+	fmt.Println("Flag A")
+	err := bow.Open(fmt.Sprintf("https://1337x.to/search/%s/1/", m.Content))
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
-	results := []string{}
+
+	var results []string
 	selector := "body > main > div > div > div > div.box-info-detail.inner-table > div.table-list-wrap > table > tbody"
 	bow.Dom().Find(selector).Find("tr").Each(func(count int, s *goquery.Selection) {
 		name := s.Find("td.coll-1").Text()
 		results = append(results, name)
 	})
+	fmt.Println(results)
 	for i := 0; i < 3; i++ {
-		s.ChannelMessageSend(channelID, fmt.Sprintf("%v. %s \n", i, results[i])) //TODO add if its a movie tv show etc
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v. %s \n", i, results[i])) //TODO add if its a movie tv show etc
 	}
-	s.ChannelMessageSend(channelID, "Pick one")
-	//(<-channel)
-	fmt.Println("Flag 1")
-	requestchannel <- -1
-	fmt.Println("Flag 2")
-	//Sends a request saying that we need a 0,1,2
-	v := <-channel //Waits until there is a -1,0,1,2
+	s.ChannelMessageSend(m.ChannelID, "Pick one")
+
+	requestchannel <- -1 //Sends a request saying that we need a 0,1,2
+	v := <-channel       //Waits until there is a -1,0,1,2
 
 	if v == -1 { //If there has been a new request then we cancel the current process
-		fmt.Println("They made a new ")
-		s.ChannelMessageSend(channelID, "Stopped the current process")
+		fmt.Println("They made a new process")
+		s.ChannelMessageSend(m.ChannelID, "Ok then")
 		return
 	}
+
 	Newselector := fmt.Sprintf("body > main > div > div > div > div.box-info-detail.inner-table > div.table-list-wrap > table > tbody > tr:nth-child(%v) > td.coll-1.name > a:nth-child(2)", v+1)
 	bow.Click(Newselector)
-	frontworkaround := "body > main > div > div > div"
-	lastworkaround := "ul:nth-child(2) > li:nth-child(1) > span"
-	egg := bow.Find(frontworkaround).Find("div.no-top-radius").Find("div").Find(lastworkaround).Text()
-	if egg == "TV" || egg == "Movies" {
-		fmt.Println("Goodboy")
-		streamselector := "#l3f908a6dc924d6f4ce84fdeea71cb688db46679c"
-		bow.Click(streamselector)
-		msg := fmt.Sprintf("%s: %s", bow.Title(), bow.Url())
-		s.ChannelMessageSendEmbed(channelID, embed.NewGenericEmbed("Your TV/Movie", msg))
+
+	//We are now in the chosen item's info page
+
+	BodyMainDivDivDiv := "body > main > div > div > div"                                                      //For reuse
+	lastworkaround := "ul:nth-child(2) > li:nth-child(1) > span"                                              //for clean
+	category := bow.Find(BodyMainDivDivDiv).Find("div.no-top-radius").Find("div").Find(lastworkaround).Text() //We find the category
+	if category == "TV" || category == "Movies" {                                                             //If it's a movie or a tv then it will serve a streamable link instead
+		bow.Find(BodyMainDivDivDiv).Find("div.no-top-radius").Find("div.clearfix").Find("ul").Find("li").Each(func(count int, selec *goquery.Selection) {
+			if selec.Find("a").Text() == "Play now (Stream)" {
+				thing2, _ := selec.Find("a").Attr("href")
+				s.ChannelMessageSendEmbed(m.ChannelID, embed.NewGenericEmbed("Your TV/Movie", thing2))
+			}
+		})
 	} else {
 		adescriptivename := "body > main > div > div > div"
 		magnetlink, _ := bow.Find(adescriptivename).Find("div.no-top-radius").Find("div.clearfix").Find("ul").Find("li").Find("a").Attr("href")
-		s.ChannelMessageSend(channelID, "Magnet Link will be sent to your dms.")
+		s.ChannelMessageSend(m.ChannelID, "Magnet Link will be sent to your dms.")
 		chann, err := s.UserChannelCreate(m.Author.ID)
 		if err != nil {
 			fmt.Println("error creating channel:", err)
